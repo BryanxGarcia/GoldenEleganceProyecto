bb712ed3b718e7f83e42c066fba71b8c87f784c0
Cristopher Bryan Bautista Garcia <92136012+BryanxGarcia@users.noreply.github.com>
2023-05-29 13:23:31 -0500

    Feat: Configuracion de entorno Feat: Modelos Initial Commit

---
+++ GoldenEleganceProyecto/Service/IServices/IUsuariosServicio.cs
@@ -0,0 +1,26 @@
+﻿
+using GoldenEleganceProyecto.Models;
+using GoldenEleganceProyecto.Models.Helpers;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.Xml;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoldenEleganceProyecto.Service.IServices
+{
+    public interface IUsuariosServicio
+    {
+        Task<List<Usuarios>> ObtenerLista();
+        Task<Usuarios> ObtenerPorId(int? Id);
+        Task<ResponseHelper> CrearUsuario(Usuarios vm);
+        Task<ResponseHelper> EditarUsuario(Usuarios vm);
+        Task<ResponseHelper> EliminarUsuario(int? Id);
+        Task<bool> ExisteUsuario(Usuarios asignatura);
+        Task<bool> ExisteUsuarioPorId(int? Id);
+
+        //Task<ResponseHelper> CargaMasiva(IFormFile excel, string connectionId);
+    }
+}
