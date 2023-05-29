cca6131c8ec0b7f6adca9da63e664a969a87ae6e
Cristopher Bryan Bautista Garcia <92136012+BryanxGarcia@users.noreply.github.com>
2023-05-29 13:23:31 -0500

    Agregar archivos de proyecto.

---
+++ GoldenEleganceProyecto/Context/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿//using ServiciosWeb.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace GoldenEleganceProyecto.Context
+{
+    public class ApplicationDbContext
+    {
+        //public class ApplicationDbContext : DbContext
+        //{
+        //    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        //        : base(options)
+        //    {
+
+        //    }
+        //    public DbSet<Rol> Roles { get; set; }
+        //    public DbSet<Categoria> Categorias { get; set; }
+        //    public DbSet<Usuarios> Usuarios { get; set; }
+        //    public DbSet<Productos> Productos { get; set; }
+        //    public DbSet<Venta> Ventas { get; set; }
+        }
+    }
+
