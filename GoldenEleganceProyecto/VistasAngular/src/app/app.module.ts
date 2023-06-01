72a4d756f239fa26cf390508ebfd52ade60adb2f
Cristopher Bryan Bautista Garcia <92136012+BryanxGarcia@users.noreply.github.com>
2023-06-01 18:19:16 -0500

    Revert "feat: Construccion del proyecto"

--- GoldenEleganceProyecto/VistasAngular/src/app/app.module.ts
+++ GoldenEleganceProyecto/VistasAngular/src/app/app.module.ts
@@ -4,28 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { LoginComponent } from './auth/pages/login/login.component';
-import { ResetPasswordComponent } from './auth/pages/reset-password/reset-password.component';
-import { ContactanosComponent } from './auth/pages/contactanos/contactanos.component';
-import { FichaProductoComponent } from './base-admin/pages/ficha-producto/ficha-producto.component';
-import { HistorialComponent } from './base-admin/pages/historial/historial.component';
-import { FavoritosComponent } from './base-admin/pages/favoritos/favoritos.component';
-import { CarritoCompraComponent } from './base-admin/pages/carrito-compra/carrito-compra.component';
-import { PerfilUsuarioComponent } from './base-admin/pages/perfil-usuario/perfil-usuario.component';
-import { InicioComponent } from './base-admin/pages/inicio/inicio.component';
-
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    ResetPasswordComponent,
-    ContactanosComponent,
-    FichaProductoComponent,
-    HistorialComponent,
-    FavoritosComponent,
-    CarritoCompraComponent,
-    PerfilUsuarioComponent,
-    InicioComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
